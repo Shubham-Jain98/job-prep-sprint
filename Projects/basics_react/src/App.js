6cad6c7d30258858bc5f9f3d9a8efbd1c15b47aa
Shubham-Jain98 <mikeysingh6969@gmail.com>
2025-05-26 16:06:29 +0530

    how to add a file such as code js

--- Projects/basics_react/src/App.js
+++ Projects/basics_react/src/App.js
@@ -1,13 +1,16 @@
+import Code from './code';
 
 function App() {
   return (
     <>
+    
     <h1>Chai or React | Shubham Jain</h1>
     <p> Difference in normal versus Vite App
       this takes more time than vite package overall
     </p>
+    <Code />
     </>
   );
 }
 
-export default App;
+export default App
--- Projects/vite_react/src/App.jsx
+++ Projects/vite_react/src/App.jsx
@@ -1,12 +1,9 @@
+import Code from './code'
 
 function App() {
 
   return (
-    <>
-      <h1>Chai or react with vite | Shubham Jain</h1>
-      <p>Why should i use this instead of normal react
-      </p>
-      </>
+      <Code />
   )
 }
 
